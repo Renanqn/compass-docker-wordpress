0e934722678d47e32947c29735eef682310c7607
Renan Nascimento <84254369+Renanqn@users.noreply.github.com>
2025-03-10 10:15:07 -0300

    Atualizado script para instalação docker e docker compose

--- user_data.sh
+++ user_data.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-yum update -y
-yum install docker -y
-systemctl start docker
-systemctl enable docker
-usermod -aG docker ec2-user
+# Atualiza pacotes
+sudo yum update -y
+
+# Instala Docker
+sudo yum install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
+
+# Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Exibe versão do Docker
+docker --version
+docker-compose --version
+
