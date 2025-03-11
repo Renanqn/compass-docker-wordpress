54c59e9fca6255928c05bb73cbd1e860c483eff9
Renan Nascimento <84254369+Renanqn@users.noreply.github.com>
2025-03-11 14:09:47 -0300

    script corrigido

--- user_data.sh
+++ user_data.sh
@@ -16,7 +16,19 @@ sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 # Instala o cliente MySQL
 sudo apt install -y mysql-client-core-8.0
 
-# Exibe versão do Docker, Docker Compose e MySQL
+# Instala o utilitário do EFS
+sudo apt install -y amazon-efs-utils
+
+# Cria o diretório para montagem do EFS
+sudo mkdir -p /mnt/efs
+
+# Monta o EFS utilizando o ID informado (fs-06ccafd3a3bb1cdbc)
+sudo mount -t efs fs-06ccafd3a3bb1cdbc:/ /mnt/efs
+
+# Adiciona o EFS ao fstab para montagem automática na reinicialização
+echo "fs-06ccafd3a3bb1cdbc:/ /mnt/efs efs defaults,_netdev 0 0" | sudo tee -a /etc/fstab
+
+# Exibe as versões instaladas (Docker, Docker Compose e MySQL Client)
 docker --version
 docker-compose --version
 mysql --version
